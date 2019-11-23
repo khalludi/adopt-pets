108f6d0748dccbe2a61c824e7b57d9565181e601
Khalid Ali <khalludi123@gmail.com>
2019-11-23 08:51:24 -0500

    Add Vuex Getters

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="home">
     <h1>Adopt a new best friend.</h1>
+    {{ getAllCats.length }}
+    {{ animalsCount }}
+
     <button @click="togglePetForm" class="btn btn-primary">Add New Pet</button>
 
-    <b-form @submit="handleSubmit" v-if="showPetForm">
+    <b-form @submit.prevent="handleSubmit" v-if="showPetForm">
       <b-form-group id="input-group-2" label="Pet's Name:" label-for="input-2">
         <b-form-input
           id="input-2"
@@ -39,6 +42,8 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
+
 export default {
   name: 'home',
   data () {
@@ -51,12 +56,35 @@ export default {
       }
     }
   },
+  computed: {
+    ...mapGetters([
+      'animalsCount',
+      'getAllCats'
+    ])
+  },
   methods: {
+    ...mapActions([
+      'addPet'
+    ]),
     togglePetForm () {
       this.showPetForm = !this.showPetForm
     },
     handleSubmit () {
+      const { species, age, name } = this.formData
+      const payload = {
+        species,
+        pet: {
+          name,
+          age
+        }
+      }
+      this.addPet(payload)
 
+      this.formData = {
+        name: '',
+        age: 0,
+        species: null
+      }
     }
   }
 }
