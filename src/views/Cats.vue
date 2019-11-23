d4959229261ac966305a2f848d97d9dca36f4352
Khalid Ali <khalludi123@gmail.com>
2019-11-23 08:25:24 -0500

    Add Vuex store for individual pet pages

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -3,7 +3,7 @@
     <h1>Cats for Adoption</h1>
     <b-table striped hover :items="cats">
       <template v-slot:cell(name)="data">
-        <router-link :to="`/pets/${data.index}`">
+        <router-link :to="`/pets/cats/${data.index}`">
           {{ data.value }}
         </router-link>
       </template>
@@ -12,13 +12,16 @@
 </template>
 
 <script>
-import cats from '@/data/cats'
+import { mapState } from 'vuex'
 
 export default {
   data () {
-    return {
-      cats
-    }
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'cats'
+    ])
   }
 }
 </script>
--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
-    Pet
+    <h1>{{ animal.name }} ({{this.$route.params.species}})</h1>
+    <p>Age: {{ animal.age }} years old</p>
+    <p>Breed: {{ animal.breed }}</p>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted () {
+    const animals = this[this.$route.params.species][this.$route.params.id]
+    this.animal = animals
+  }
+}
+</script>
